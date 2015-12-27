58b763613743242d02612b5b431aa8aab01ac62b
Uice Lu <uicestone@gmail.com>
2015-12-28 02:28:33 +0800

    支持动态订阅股价

--- app/events/market.js
+++ app/events/market.js
@@ -1,28 +1,48 @@
 var http = require('http');
 
+var subscribes = [];
+
+var url = 'http://xueqiu.com/v4/stock/quotec.json?code=';
+
 module.exports = function(io, quotes) {
+
+    var handle = function(data) {
+        var quotesLatest = JSON.parse(data.toString());
+        var quotesHasUpdate = {};
+        for(var key in quotesLatest) {
+
+            if(quotes[key] && quotes[key][0] === quotesLatest[key][0]) {
+                continue;
+            }
+
+            quotes[key] = quotesLatest[key];
+            quotesHasUpdate[key] = quotesLatest[key];
+        }
+
+        if(Object.keys(quotesHasUpdate).length > 0){
+            io.sockets.send(quotes);
+            console.log('Data sent:', quotes);
+        }
+    }
+
     setInterval(function() {
-        http.get('http://xueqiu.com/v4/stock/quotec.json?code=SH000001,NASDAQ,BTCNCNY', function(res){
-
-            res.on('data', function (chunk) {
-
-                var quotesLatest = JSON.parse(chunk.toString());
-                var quotesHasUpdate = {};
-                for(var key in quotesLatest) {
-
-                    if(quotes[key] && quotes[key][0] === quotesLatest[key][0]) {
-                        continue;
-                    }
-
-                    quotes[key] = quotesLatest[key];
-                    quotesHasUpdate[key] = quotesLatest[key];
-                }
-
-                if(Object.keys(quotesHasUpdate).length > 0){
-                    io.sockets.send(quotes);
-                    console.log('Data sent.', quotes);
-                }
+        if(!subscribes.length) {
+            return;
+        }
+        http.get(url + subscribes.join(), function(res){
+            var data = '';
+            res.on('data', function(chunk) {
+                data += chunk;
             });
+            res.on('end', function() {
+                handle(data.toString());
+            })
         });
     }, 300);
+
+    return {
+        subscribe: function(newSubscribes) {
+            subscribes = newSubscribes;
+        }
+    };
 };
