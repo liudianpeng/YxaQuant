b4e577fabfd543a11825be23fd2e6025735e3ea3
Uice Lu <uicestone@gmail.com>
2015-12-30 01:33:52 +0800

    交易接口测试交互

--- app/events/trade.js
+++ app/events/trade.js
@@ -1,15 +1,64 @@
 module.exports = function(socketServer) {
-    socketServer.on('connection', function(socket){
+
+    var socket;
+
+    socketServer.on('connection', function(s){
+
+        if(socket) {
+            socket.write('New connection from ' + socket.remoteAddress + '. Closing this connection.');
+            socket.end();
+        }
+
+        socket = s;
         
         console.log('[' + new Date() + '] Client ' + socket.remoteAddress + ' connected to socket server.');
+        socketServer.getConnections((err, count) => console.log('[' + new Date() + '] Server has now ' + count + ' connections.'));
 
         socket.on('data', function(data){
-            
+            console.log('[' + new Date() + '] Data received from client ' + socket.remoteAddress + ': ' + data.toString());
         });
 
         socket.on('end', function(){
-            
+            console.log('[' + new Date() + '] Client ' + socket.remoteAddress + ' disconnected from socket server.');
+            socketServer.getConnections((err, count) => console.log('[' + new Date() + '] Server has now ' + count + ' connections.'));
+            socket = null;
         });
+
+        // 发送挂单信息
+        setTimeout(function(){
+            console.log('[' + new Date() + '] Sending declaration...');
+            var data = {
+                id: '5654a6aa2904b82f89a7b5b3',
+                name: '易鑫安1期',
+                provider: {
+                    name: '中信信托', // 通道提供商名称
+                    code: 'ZXXT' // 通道提供商代号
+                },
+                credentials: {
+                    login: '12345678', // 登录帐号
+                    pass: '111111' // 密码
+                },
+                declarations: [ // 量化服务向交易接口服务发送的报单/撤单后的所有申报，交易接口服务向量化服务发送的报单/撤单/成交后的所有申报
+                    {
+                        id: '5654a6aa2904b82f89a7b5b4',
+                        stock: {
+                            id: '5654a6aa2904b82f89a7b5b5',
+                            type: 'stock',
+                            name: '浦发银行', // 标的的显示名称
+                            code: 'SH600000', // 标的的代号
+                        },
+                        type: '', // 挂单类型
+                        serialNumber: null, // 通道流水号/合同号
+                        price: 18.70,
+                        time: '2015-12-28T07:00:00.000Z',
+                        volume: 10000, // 正数买入, 负数卖出
+                        volumeCompleted: 0, // 正数买入, 负数卖出
+                        status: 'not declared'
+                    }
+                ],
+            };
+            socket.write(JSON.stringify(data));
+        }, 3000);
     })
     .on('error', function (e) {
         console.log('[' + new Date() + '] Socket server error:', e);
---
+++ trade-test.js
@@ -0,0 +1,53 @@
+var net = require('net');
+
+var client = net.connect(8888, function() {
+    console.log('[' + new Date() + '] Connected to server.');
+});
+
+client.on('data', function(data) {
+    console.log('[' + new Date() + '] Data received: ', data.toString());
+    
+    var data = JSON.parse(data);
+
+    // 发送报单结果
+    setTimeout(function(){
+        data.declarations = data.declarations.map(function(declaration){
+            if(!declaration.serialNumber) {
+                declaration.serialNumber = 10001;
+            }
+            declaration.status = 'declared';
+            return declaration;
+        });
+        console.log('[' + new Date() + '] Sending declaration results...');
+        client.write(JSON.stringify(data));
+    }, 1000);
+    
+    // 发送成交结果
+    setTimeout(function(){
+
+        // 模拟第一笔申报成交
+        var declaration = data.declarations[0];
+        declaration.volumeCompleted = declaration.volume / 2;
+
+        // 其中又分为40%, 60%两笔成交
+        [0.4, 0.6].forEach(ratio => {
+            var transaction = {};
+            Object.assign(transaction, declaration);
+            transaction.volume = declaration.volumeCompleted * ratio;
+            transaction.amount = transaction.volume * transaction.price;
+            declaration.status = 'partial completed';
+            if(!data.transactions){
+                data.transactions = [];
+            }
+            data.transactions.push(transaction);
+        });
+
+        console.log('[' + new Date() + '] Sending transaction results...');
+        client.write(JSON.stringify(data));
+
+    }, 5000);
+});
+
+client.on('end', function() {
+    console.log('[' + new Date() + '] Disconnected from server.');
+});
