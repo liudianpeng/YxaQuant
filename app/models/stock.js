c14dc44a5222da275b3d7646183481cee9d22cf7
Uice Lu <uicestone@gmail.com>
2015-12-30 15:10:11 +0800

    下载雪球股票数据的脚本

--- app/models/stock.js
+++ app/models/stock.js
@@ -1,49 +1,50 @@
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 
-var StockSchema = new Schema({
+var stockSchema = new mongoose.Schema({
     id: String,
     type: String,
     name: String, // 标的的显示名称
     code: String, // 标的的代号
     currency: String, // 币种，仅针对有价证券
-    price: String, // 现价
-    percentage: String, // 现价/昨收偏离率
-    priceOpen: String, // 今日开盘价格
-    priceLastClose: String, // 昨日收盘价格
-    priceTodayMax: String, // 盘中最高价
-    priceTodayMin: String, // 盘中最低价
-    priceRiseStop: String, // 涨停价
-    priceFallStop: String, // 跌停价
-    amount: String, // 成交额
-    volume: String, // 成交股数
-    quoteQueue:{
+    current: Number, // 现价
+    percentage: Number, // 现价/昨收偏离率
+    open: Number, // 今日开盘价格
+    lastClose: Number, // 昨日收盘价格
+    todayMax: Number, // 盘中最高价
+    todayMin: Number, // 盘中最低价
+    riseStop: Number, // 涨停价
+    fallStop: Number, // 跌停价
+    amount: Number, // 成交额
+    volume: Number, // 成交股数
+    quoteQueue: {
         buy:[
             {
-                price:String, // 申报买入价
-                volume:String // 申报买入股数
+                price:Number, // 申报买入价
+                volume:Number // 申报买入股数
             }
         ],
         sell:[
             {
-                price:String,
-                volume:String
+                price:Number,
+                volume:Number
             }
         ]
     },
-    pb: String, // 市净率
-    peLyr: String, // 上年度市盈率
-    peTtm: String, // 本季度市盈率
-    psr: String, // 市销率
-    totalShares: String, // 总手数
-    floatShares: String, // 流通手数
-    marketCapital: String, // 总市值
-    floatMarketCapital: String, // 流通市值
-    netAssets: String, // 每股净资产
-    tickSize: String, // 最小价格单位
-    lotSize: String, // 每手股数
+    pb: Number, // 市净率
+    peLyr: Number, // 上年度市盈率
+    peTtm: Number, // 本季度市盈率
+    psr: Number, // 市销率
+    totalShares: Number, // 总手数
+    floatShares: Number, // 流通手数
+    marketCapital: Number, // 总市值
+    floatMarketCapital: Number, // 流通市值
+    netAssets: Number, // 每股净资产
+    tickSize: Number, // 最小价格单位
+    lotSize: Number, // 每手股数
     industry: [String], // 行业
     concept: [String] // 概念
 });
 
-module.exports = mongoose.model('Stock', StockSchema);
+stockSchema.index({code:1}, {unique:true});
+
+module.exports = mongoose.model('Stock', stockSchema);
---
+++ download-stocks.js
@@ -0,0 +1,127 @@
+var http = require('http');
+var _ = require('lodash');
+var mongoose = require('mongoose');
+var Stock = require('./app/models/stock');
+var currentPage = 1, connections = 0, maxConnections = 20, totalCodes = [];
+
+mongoose.connect('mongodb://localhost/yxaquant');
+
+http.get('http://xueqiu.com', function(res){
+
+    var cookies = res.headers['set-cookie'];
+    var timestart = new Date().getTime();
+    var downloadStockListInterval = setInterval(function(){
+
+        if(connections >= maxConnections) {
+            return;
+        }
+
+        connections++;
+        // console.log('connections:', connections);
+
+        http.get({
+            hostname: 'xueqiu.com',
+            path: '/stock/screener/screen.json?category=SH&orderby=symbol&order=asc&page=' + currentPage,
+            headers: {
+                cookie: cookies
+            }
+        }, function(res){
+            var data = '';
+
+            res.on('data', function(chunk){
+                data += chunk;
+            });
+
+            res.on('end', function(){
+                connections--;
+
+                var codes = JSON.parse(data.toString()).list.map(item => item.symbol);
+                
+                totalCodes = totalCodes.concat(codes);
+
+                if(codes.length === 0) {
+                    // console.log('Interval cleared: downloadStockListInterval');
+                    clearInterval(downloadStockListInterval);
+                    return;
+                }
+                
+                if(connections === 0) {
+                    console.log('Total: ' + totalCodes.length, 'Time: ' + (new Date().getTime() - timestart));
+
+                    // 开始股票信息下载
+                    maxConnections = 40;
+                    timestart = new Date().getTime();
+                    var index = 0, stocksRequested = 0, stocksSaved = 0;
+                    var downloadStockInterval = setInterval(function(){
+                        var code = totalCodes[index];
+
+                        if(connections >= maxConnections) {
+                            return;
+                        }
+
+                        connections++;
+                        stocksRequested++;
+                        // console.log('Getting ' + code + '...');
+                        http.get({
+                            hostname: 'xueqiu.com',
+                            path: '/v4/stock/quote.json?code=' + code,
+                            headers: {
+                                cookie: cookies
+                            }
+                        }, function(res){
+                            var data = '';
+                            res.on('data', function(chunk){
+                                data += chunk;
+                            });
+                            res.on('end', function(){
+                                connections--;
+                                var stockData = JSON.parse(data)[code];
+
+                                if(!stockData.code || !stockData.name) {
+                                    console.error('Failed at ' + code, stockData, data);
+                                    return;
+                                }
+
+                                stockData = _.mapKeys(stockData, function(value, key) {
+                                    return _.camelCase(key);
+                                });
+
+                                stockData.code = stockData.symbol;
+                                stockData.currency = stockData.currencyUnit;
+                                stockData.type = 'stock';
+
+                                Stock.findOneAndUpdate(
+                                    {code: stockData.code}, 
+                                    stockData,
+                                    {new: true, upsert: true}, 
+                                    function(err, result) {
+                                        err && console.error('Error saving', stockData.code, err);
+                                        stocksSaved++;
+                                        if(stocksRequested === totalCodes.length) {
+                                            console.log('Stocks downloaded: ' + stocksSaved, 'Time: ' + (new Date().getTime() - timestart));
+                                            mongoose.disconnect();
+                                        }
+                                    }
+                                );
+                                
+                                // console.info('Saved:', stock.code, stock.name);
+                            })
+                        });
+    
+                        if(index === totalCodes.length - 1) {
+                            clearInterval(downloadStockInterval);
+                            // console.log('Interval cleared: downloadStockInterval');
+                            return;
+                        }
+
+                        index ++;
+                    });
+                    // mongoose.disconnect();
+                }
+            });
+        });
+
+        currentPage++;
+    });
+    
+});
