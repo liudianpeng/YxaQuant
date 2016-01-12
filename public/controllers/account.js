a22581f28b04f72f506c938d852a576246aa2b91
Uice Lu <uicestone@gmail.com>
2016-01-13 01:04:19 +0800

    前端分页参数获得示例

--- public/controllers/account.js
+++ public/controllers/account.js
@@ -4,6 +4,15 @@ angular.module('yxaquant.account', [])
 
 	$scope.accounts = accounts;
 
+	// 从后端获得分页参数示例，请求支持使用skip和limit来分页，limit默认20
+	// 对于数据量不大的account, user, config也可使用前端分页
+	$scope.list = {
+		start: +accounts.$response.headers('items-start'),
+		end: +accounts.$response.headers('items-end'),
+		total: +accounts.$response.headers('items-total')
+	}
+	console.log($scope.list);
+
 }])
 
 .controller('AccountDetailController', ['$scope', function ($scope) {
