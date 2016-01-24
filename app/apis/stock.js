b0dd61c07dc25c87482b2224582e8776a04d2504
Uice Lu <uicestone@gmail.com>
2016-01-25 01:41:01 +0800

    股票列表接口支持参数查询和排序

--- app/apis/stock.js
+++ app/apis/stock.js
@@ -1,4 +1,5 @@
 var Stock = require('../models/stock.js');
+var StockGroup = require('../models/stockGroup.js');
 
 module.exports = function(router) {
     // Stock CURD
@@ -28,21 +29,73 @@ module.exports = function(router) {
             var limit = +req.query.limit || 20;
             var skip = +req.query.skip || 0;
 
-            var query = {};
+            var queryPromises = [];
+            var query = Stock.find().limit(limit).skip(skip);
 
             if(req.query.keyword) {
-                query = {
-                    $or: [
-                        {name: new RegExp(req.query.keyword)},
-                        {code: new RegExp(req.query.keyword)}
-                    ]
-                };
+                query.find({$or: [
+                    {name: new RegExp(req.query.keyword)},
+                    {code: new RegExp(req.query.keyword)}
+                ]});
             }
 
-            var data = Stock.find(query)
-            .limit(limit)
-            .skip(skip)
-            .exec()
+            ['code', 'name'].forEach(property => {
+                if(req.query[property]) {
+                    query.find({[property]: new RegExp(req.query[property])});
+                }
+            });
+
+            ['percentage', 'pb', 'peTtm', 'peLyr', 'psr', 'marketCapital', 'floatMarketCapital'].forEach(property => {
+                
+                if(req.query[property]) {
+
+                    var range = req.query[property].split('-');
+                    var min = Number(range[0]), max = Number(range[1]);
+                    var condition = {[property]:{}};
+
+                    if(min) {
+                        condition[property].$gte = min;
+                    }
+
+                    if(max) {
+                        condition[property].$lte = max;
+                    }
+
+                    query.find(condition);
+
+                }
+
+            })
+
+            if(req.query.groupId) {
+
+                queryPromises.push(new Promise((resolve, reject) => {
+
+                    StockGroup.findOne({_id: req.query.groupId}, function(err, result) {
+                        
+                        var stockIds = result.stocks.map(stock => stock.id);
+
+                        query.find({_id: {$in: stockIds}});
+
+                        resolve();
+                    })
+
+                }));
+
+            }
+
+            if(req.query.orderBy) {
+                query.sort({
+                    [req.query.orderBy]: (req.query.order === 'desc' || req.query.order === 'false' || Number(req.query.order) <= 0 ? 'desc' : 'asc')
+                });
+            }
+
+            Promise.all(queryPromises)
+
+            .then(() => {
+                return query.exec();
+            })
+
             .then(result => {
 
                 if(skip + result.length > Stock.totalCount) {
@@ -54,7 +107,6 @@ module.exports = function(router) {
                 .set('Items-End', Math.min(skip + limit, Stock.totalCount))
                 .json(result);
             });
-
 
         });
 
